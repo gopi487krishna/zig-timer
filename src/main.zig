4ed9ccdaa6f59f389c8d01e2cff46d92b06214f4
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-02 21:29:13 +0530

    Implement main with arg validation for running timer

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,21 @@
 const std = @import("std");
+const expect = std.testing.expect;
+const timer = @import("./root.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    if (args.len < 2) {
+        std.debug.print("Usage : zig-timer-waybar <duration>\n", .{});
+        std.process.abort();
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // Second argument represents the amount of time(in seconds)
+    const seconds = try std.fmt.parseInt(u64, args[1], 10);
+    try timer.validateSeconds(seconds);
+    try timer.runTimer(seconds);
 }
